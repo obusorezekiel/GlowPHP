0b51d39145dc42fcab05e14c898d9901ba1f0779
obusorezekiel <obusorezekiel@gmail.com>
2017-11-03 12:45:47 +0100

    Added Views

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -6,7 +6,8 @@ class Home extends Controller{
     public function index($name = ''){
         $user = $this->model('User');
         $user->name = $name;
-        echo $user->name;
+
+        $this->view('home/index', ['name' => $user->name]);
     }
 }
 
